64b1f3b93ca0734abc2f53ee4294b7099a3e968c
PaS76 <patrick.suwito@ma-design.de>
2014-03-11 14:44:28 +0100

    [Mapper]: First implementation snapshot.

--- source/mattt.application/mattt.mapping/Mapper.cs
+++ source/mattt.application/mattt.mapping/Mapper.cs
@@ -5,9 +5,31 @@ namespace mattt.mapping
 {
     public class Mapper
     {
+        private GameState _gameState;
+        private readonly string _status = string.Empty;
+        private const int DIM = 3;
+
+        public Mapper()
+        {
+            Reset();
+        }
+
         public GameState Map(Tuple<int, char>[] playerMoves, string status)
         {
-           return new GameState{Board=new char[3,3], Status = string.Empty};
+            foreach (var playerMove in playerMoves)
+            {
+                var posX = playerMove.Item1 % DIM;
+                int posY;
+                Math.DivRem(playerMove.Item1, DIM, out posY);
+                _gameState.Board[posX, posY] = playerMove.Item2;
+            }
+            _gameState.Status = status;
+            return _gameState;
+        }
+
+        private void Reset()
+        {
+            _gameState = new GameState { Board = new char[DIM, DIM], Status = _status };
         }
     }
 }
